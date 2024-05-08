fb858b34068bc4ee3f5e546b6c164cc0de49de49
Vadim Yatskul <vadimajj@gmail.com>
2024-05-08 15:39:53 +0300

    bug fixes

--- src/components/sliderCenter/SliderCenterSection.jsx
+++ src/components/sliderCenter/SliderCenterSection.jsx
@@ -82,37 +82,3 @@ const SliderCenterSection = () => {
 };
 
 export default SliderCenterSection;
-
-// return (
-//     <section className="SliderCenterSection mb-16 md:mb-36 overflow-hidden">
-//       <div className="container mx-auto">
-//         <div className="card__content sm:mb-5 lg:mb-0 xsm:ml-[80px] xsm:w-[600px]  sm:min-w-[800px]   md:min-w-[calc((100%_-_40px)*1.3_+_60px)]  lg:ml-[calc((100%_-_100px)/6_+_20px)]">
-//           <swiper-container
-//             className="  sm:max-w-[640px] md:w-[768px] lg:w-[960px]"
-//             ref={swiperRef}
-//             init="false"
-//           >
-//             {slides.map((slide) => (
-//               <swiper-slide
-//                 key={slide.id}
-//                 className="   sm:max-w-[calc((100%_-_40px)/2)]  md:min-w-[calc((100%_-_40px)/3)]"
-//               >
-//                 <SliderItem slide={slide} />
-//               </swiper-slide>
-//             ))}
-//           </swiper-container>
-//         </div>
-//         <div className="hidden lg:block">
-//           <SliderNavButtons
-//             onNextSlide={handleNext}
-//             onPrevSlide={handlePrev}
-//             className="hover:fill-accent transition-color duration-[250ms]"
-//           />
-//         </div>
-//         <Button type="centerSliderSection">Show me more</Button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default SliderCenterSection;
