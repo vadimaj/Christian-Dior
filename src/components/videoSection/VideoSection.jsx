e1e1dfedd7995357ab304acde6a981a2cadb0171
Vadim Yatskul <vadimajj@gmail.com>
2024-07-11 22:51:33 +0300

    gh-pages update

--- src/components/videoSection/VideoSection.jsx
+++ src/components/videoSection/VideoSection.jsx
@@ -4,7 +4,7 @@ const VideoSection = () => {
     <div className="video mb-[150px]">
       <div className="container mx-auto">
         <div className=" gap-x-5 lg:grid lg:grid-cols-6">
-          <div className="  min-h-[300px] bg-no-repeat bg-cover bg-center flex items-center justify-center  lg:col-start-2 lg:col-span-5 bg-[url('src/assets/images/video/video_bg-mob.jpg')]  md:bg-[url('src/assets/images/video/video_bg-tab.jpg')] md:min-h-[420px] lg:bg-[url('src/assets/images/video/video_bg.jpg')] lg:min-h-[600px]">
+          <div className="  min-h-[300px] bg-no-repeat bg-cover bg-center flex items-center justify-center  lg:col-start-2 lg:col-span-5 bg-[url('./assets/images/video/video_bg-mob.jpg')]  md:bg-[url('./assets/images/video/video_bg-tab.jpg')] md:min-h-[420px] lg:bg-[url('./assets/images/video/video_bg.jpg')] lg:min-h-[600px]">
             <button>
               <Icon id="play" width="100" height="100" className="" />
             </button>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
+import copy from 'rollup-plugin-copy';
+
 export default defineConfig({
   base: '/Christian-Dior/',
-  plugins: [react(), eslint],
+  plugins: [
+    react(),
+    eslint,
+
+    copy({
+      targets: [
+        {
+          src: 'src/assets/images/products/*.jpg',
+          dest: 'dist/assets/images/products',
+        },
+        {
+          src: 'src/assets/images/products/*.png',
+          dest: 'dist/assets/images/products',
+        },
+      ],
+      verbose: true,
+    }),
+  ],
 });
